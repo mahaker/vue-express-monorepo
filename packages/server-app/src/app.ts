ee9f82796439a3f89c021e244ae13aa493d554ab
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2022-02-05 14:47:13 +0900

    server-app depends on core

--- packages/server-app/src/app.ts
+++ packages/server-app/src/app.ts
@@ -1,7 +1,10 @@
-import express = require('express')
+import express from 'express'
+import * as core from 'core'
+
 const app = express()
 
 app.get('/api/hello', (req, res) => {
+  console.info('Access!', core.add(2, 4))
   res.json({ message: 'Hello World!' })
 })
 
